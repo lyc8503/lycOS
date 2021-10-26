46cba9ffe89b8ea26eec34e4b92ad9570778a8f4
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2021-10-26 15:03:44 +0800

    fifo 8bit buffer.(circular linked list)

--- lycOS/bootpack.c
+++ lycOS/bootpack.c
@@ -11,6 +11,15 @@ void MyOSMain() {
     // 读取启动信息
     struct BOOTINFO *binfo = (struct BOOTINFO *) BOOTINFO_ADDR;
 
+    // 初始化 gdt 和 idt
+    init_gdtidt();
+    // 初始化 pic
+    init_pic();
+
+    io_sti();  // CPU 接收中断
+    io_out8(PIC0_IMR, 0xf9);  // 11111001 接收 PIC1 和键盘中断
+    io_out8(PIC1_IMR, 0xef);  // 11101111 接收鼠标中断
+
 
     // 初始化调色板
     init_palette();
@@ -21,14 +30,6 @@ void MyOSMain() {
     // 图片显示 HelloWorld!
     put_ascii_str8(binfo->vram, binfo->scrnx, 8, 8, COLOR8_WHITE, "HelloWorld from lycOS!");
 
-    // 初始化 gdt 和 idt
-    init_gdtidt();
-    // 初始化 pic
-    init_pic();
-
-    io_sti();  // CPU 接收中断
-    io_out8(PIC0_IMR, 0xf9);  // 11111001 接收 PIC1 和键盘中断
-    io_out8(PIC1_IMR, 0xef);  // 11101111 接收鼠标中断
 
     while(1){
         io_hlt();
