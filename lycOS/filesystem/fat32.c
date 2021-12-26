5829eaeeeb490919d2c1bd168acf42f73002fc83
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2021-12-26 22:48:51 +0800

    fat32 still wip.

--- lycOS/filesystem/fat32.c
+++ lycOS/filesystem/fat32.c
@@ -54,7 +54,34 @@ typedef struct FSINFO {
     uint8_t reserved2[12];
     uint32_t trail_signature;
 } FSINFO;
+
+typedef struct STANDARD83 {
+    uint8_t filename[11];
+    uint8_t attrib;
+    uint8_t reserved_for_nt;
+    uint8_t creation_time_tenth;
+    uint16_t creation_time;
+    uint16_t creation_date;
+    uint16_t last_accessed;
+    uint16_t cluster_high;
+    uint16_t last_modification_time;
+    uint16_t last_modification_date;
+    uint16_t cluster_low;
+    uint32_t size;
+} STANDARD83;
+
+typedef struct LONG_FILENAME {
+    uint8_t order;
+    uint8_t character1[10];
+    uint8_t attrib;
+    uint8_t entry_type;
+    uint8_t checksum;
+    uint8_t character2[12];
+    uint16_t zero;
+    uint8_t character3[4];
+} LONG_FILENAME;
 #pragma pack()
+
 
 // 读取一个 FAT32 分区
 FAT32_PARTITION *new_fat32_partition(DRIVE *drv, uint32_t sector_start, uint32_t sectors) {
@@ -85,12 +112,13 @@ FAT32_PARTITION *new_fat32_partition(DRIVE *drv, uint32_t sector_start, uint32_t
         return NULL;
     }
 
-    trace_fat32("sectors_per_cluster: %d, root_cluster_num: %d, reserved_sector_count: %d, fat count: %d, sectors per fat: %d",
-                bpb->sectors_per_cluster,
-                ebpb->root_cluster_num,
-                bpb->reserved_sector_count,
-                bpb->fat_count,
-                ebpb->sectors_per_fat);
+    trace_fat32(
+            "sectors_per_cluster: %d, root_cluster_num: %d, reserved_sector_count: %d, fat count: %d, sectors per fat: %d",
+            bpb->sectors_per_cluster,
+            ebpb->root_cluster_num,
+            bpb->reserved_sector_count,
+            bpb->fat_count,
+            ebpb->sectors_per_fat);
 
     partition->sectors_per_cluster = bpb->sectors_per_cluster;
     partition->root_cluster_num = ebpb->root_cluster_num;
@@ -114,19 +142,20 @@ uint32_t get_cluster_start(FAT32_PARTITION *partition, int cluster) {
 }
 
 void iterate_fat(FAT32_PARTITION *partition) {
-    int start_sector = partition->reserved_sector_count + partition->sector_start;
-    uint8_t *buf = (uint8_t *) memman_alloc_4k(sys_memman, 512);
-    if (buf == NULL) {
-        // TODO
-    }
-
-    trace_fat32("start: %d", start_sector);
-
-    read_drive(partition->drv, buf, start_sector, 1);
-
+//    int start_sector = partition->reserved_sector_count + partition->sector_start;
+//    uint8_t *buf = (uint8_t *) memman_alloc_4k(sys_memman, 512 * 8);
+//    if (buf == NULL) {
+//        // TODO
+//    }
+//
+//    read_drive(partition->drv, buf, start_sector, 8);
+//
 //    int i;
-//    for (i = 0; i < 512; i++) {
-//        printk("%02x ", buf[i]);
+//
+//    uint32_t *fat = buf;
+//
+//    for (i = 512 * 2 / 8; i < 512; i++) {
+//        printk("%08x\n", *(fat + i));
 //    }
 
 }
@@ -135,12 +164,29 @@ void list_directories(FAT32_PARTITION *partition) {
     int start_sector = get_cluster_start(partition, partition->root_cluster_num) + partition->sector_start;
     uint8_t *buf = (uint8_t *) memman_alloc_4k(sys_memman, 512 * partition->sectors_per_cluster);
 
-    trace_fat32("start: %d", start_sector);
-
     read_drive(partition->drv, buf, start_sector, partition->sectors_per_cluster);
 
     int i;
-    for (i = 0; i < 512 * partition->sectors_per_cluster; i++) {
-        printk("%02x ", buf[i]);
+    for (i = 0; i < 512 * partition->sectors_per_cluster; i += 32) {
+        int j;
+        for (j = 0; j < 32; j++) {
+            printk("%02x ", buf[i + j]);
+        }
+        printk("\r\n");
+
+        STANDARD83 *entry = (STANDARD83 *) (buf + i);
+
+        if ((entry->attrib & 0x0F) == 0x0F) {
+            trace_fat32("Long file name.");
+        }
+
+        if (entry->attrib & 0x10) {
+            trace_fat32("Directory.");
+        }
+
+        // Archive bit is ignored here.
+
+        printk("%s", entry->filename);
+        printk("\r\n");
     }
 }
